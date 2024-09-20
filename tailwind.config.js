0cba6155a37c263a98e47c427d167c877b98e36c
unknown <ozeias.ssilva@gmail.com>
2024-09-19 23:35:08 -0300

    continando o projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,33 @@
 module.exports = {
   content: ['*.{html,js}'],
   theme: {
-    extend: {},
+    container: {
+      padding:{
+        DEFAULT: '15px'
+      }
+    },
+    screens: {
+     sm: '640px',
+     md: '768px',
+     lg: '960px',
+     xl: '1330px',     
+    },
+    extend: {
+      colors:{
+      primary: '242a2b',
+      secondary: '#808080',
+      accent: {
+        DEGAULT: '#1cbccf',
+        secondary: '#18abbc',
+        tertiary: '#90c6cd',
+      },
+       grery: '#e8f0f1',
+      },
+      fontFamily:{
+      primary: 'Poppins'
+      },
+      
+    },
   },
   plugins: [],
-}
-
+};
